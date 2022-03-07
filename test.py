a8074dd27fcbe33044054543b0bc51cfdf6d842a
David Markey <david@dmarkey.com>
2022-03-07 21:27:50 +0000

    Add fallback for gzip kernel

--- test.py
+++ test.py
@@ -2,15 +2,18 @@
 import tarfile
 import urllib.request
 import gzip
-urllib.request.urlretrieve("https://cloud-images.ubuntu.com/releases/focal/release/unpacked/ubuntu-20.04-server-cloudimg-arm64-vmlinuz-generic", "kernel.gz")
+urllib.request.urlretrieve("https://cloud-images.ubuntu.com/releases/focal/release/unpacked/ubuntu-20.04-server-cloudimg-amd64-vmlinuz-generic", "kernel")
 urllib.request.urlretrieve("https://cloud-images.ubuntu.com/releases/focal/release/unpacked/ubuntu-20.04-server-cloudimg-amd64-initrd-generic", "initrd")
 urllib.request.urlretrieve("https://cloud-images.ubuntu.com/releases/focal/release/ubuntu-20.04-server-cloudimg-amd64.tar.gz", "image.tar.gz")
 tf = tarfile.open("image.tar.gz")
 tf.extractall()
 tf.close()
-kern = gzip.open("kernel.gz")
-uncompressed = kern.read()
-kern.close()
-with open("kernel", "wb") as f:
-    f.write(uncompressed)
+try:
+    kern = gzip.open("kernel")
+    uncompressed = kern.read()
+    kern.close()
+    with open("kernel", "wb") as f:
+        f.write(uncompressed)
+except gzip.BadGzipFile:
+    pass
 
